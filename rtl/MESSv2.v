cefe59f42f1b47c137f0eb51fa90127be5c2fa61
Patrick Allison <allison.122@osu.edu>
2014-11-12 12:24:55 -0500

    3.8.27 - add reset to command path.

--- rtl/MESSv2.v
+++ rtl/MESSv2.v
@@ -47,11 +47,11 @@ module MESSv2( input        clk_i,
 	       );
 
 	localparam [31:0] IDENT = "SURF";
-	localparam [3:0] VER_MONTH = 9;
-	localparam [7:0] VER_DAY = 19;
+	localparam [3:0] VER_MONTH = 11;
+	localparam [7:0] VER_DAY = 12;
 	localparam [3:0] VER_MAJOR = 3;
 	localparam [3:0] VER_MINOR = 8;
-	localparam [7:0] VER_REV = 24;
+	localparam [7:0] VER_REV = 27;
 	localparam [3:0] VER_BOARDREV = 0;
    localparam [31:0] VERSION = {VER_BOARDREV,VER_MONTH,VER_DAY,VER_MAJOR,VER_MINOR,VER_REV};
 
--- rtl/SURF_command_receiver_v3.v
+++ rtl/SURF_command_receiver_v3.v
@@ -58,7 +58,8 @@ module SURF_command_receiver_v3(
 	// all commands are transmitted, then Clr_All is issued which dumps the
 	// event buffers on both the SURF and the TURF. Then triggers are re-enabled.
 	always @(posedge clk33_i) begin
-		case (state)
+		if (rst_i) state <= IDLE; 
+		else case (state)
 			IDLE: if (data_available && data_out == TX_HEADER) state <= LAB_ID;
 			LAB_ID: if (data_available) state <= EVENT_ID_3;
 			EVENT_ID_3: if (data_available) state <= EVENT_ID_2;
@@ -98,7 +99,7 @@ module SURF_command_receiver_v3(
 	uart_rx u_receiver(.serial_in(cmd_in_sync),
 							 .data_out(data_out),
 							 .read_buffer(data_available),
-							 .reset_buffer(1'b0),
+							 .reset_buffer(rst_i),
 							 .en_16_x_baud(1'b1),
 							 .buffer_data_present(data_available),
 							 .buffer_full(),
